739b80152463e426eae40097fd845fd46625475c
Manuch Sadri <colonquebang@hotmail.com>
2023-08-07 22:28:49 -0700

    updated models.py and cookie_stand_create form debugged

--- cookie_stand/views_front.py
+++ cookie_stand/views_front.py
@@ -25,7 +25,8 @@ class CookieStandUpdateView(LoginRequiredMixin, UpdateView):
 class CookieStandCreateView(LoginRequiredMixin, CreateView):
     template_name = "cookie_stand/cookie_stand_create.html"
     model = CookieStand
-    fields = ["name", "rating", "reviewer"]  # "__all__" for all of them
+    # fields = ["name", "rating", "reviewer"]  # "__all__" for all of them
+    fields = "__all__"
 
 
 class CookieStandDeleteView(LoginRequiredMixin, DeleteView):
